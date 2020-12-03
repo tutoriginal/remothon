1b75bb4c9963d36289a330f979827f1e5ee4f348
Antoine Coulon <antoine@30c27b.com>
2020-12-03 15:49:04 +0100

    added option to run part of the program

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-require('./control/main');
-require('./dashboard/server');
+const config = require('./config.json');
+
+if (config.control.run) require('./control/main');
+if (config.dashboard.run) require('./dashboard/server');
